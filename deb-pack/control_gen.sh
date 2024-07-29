ac68a55e0e9bf24d842764d32f517d032475a9a0
AsenHu <78863300+AsenHu@users.noreply.github.com>
2024-07-29 05:45:01 +0000

    chore: Update release workflow to include building and packaging caddy for different versions and architectures

---
+++ deb-pack/control_gen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cat <<EOF
+Package: caddy
+Version: $1
+Architecture: amd64
+Maintainer: Asen Hu <caddy@asen.page>
+Description: Fast and extensible multi-platform HTTP/1-2-3 web server with automatic HTTPS
+EOF
