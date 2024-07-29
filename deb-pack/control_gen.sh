c065da5832a33a2624170b62fe02d523ab290985
AsenHu <78863300+AsenHu@users.noreply.github.com>
2024-07-29 05:53:47 +0000

    chore: Update release workflow to include updating the version in the DEBIAN control file

--- deb-pack/control_gen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat <<EOF
-Package: caddy
-Version: $1
-Architecture: amd64
-Maintainer: Asen Hu <caddy@asen.page>
-Description: Fast and extensible multi-platform HTTP/1-2-3 web server with automatic HTTPS
-EOF
